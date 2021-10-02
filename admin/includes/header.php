fd2ab2086aafbb17e944e6b56cdc7903411c8da9
arun347 <kanjoorarun@gmail.com>
2021-10-02 23:17:37 +0530

    fixed the datatables script tag

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -30,18 +30,18 @@
 <!-- jquery validate -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- DataTables  & Plugins -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="../../code/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../code/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../../code/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../../code/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../../code/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../../code/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../../code/plugins/jszip/jszip.min.js"></script>
+<script src="../../code/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../../code/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../../code/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../../code/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../../code/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- CSS only -->
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> -->
 <!-- JavaScript Bundle with Popper -->
