c1ef537cb96f40688fce625298d54fe7495d4914
arun347 <kanjoorarun@gmail.com>
2021-10-06 18:13:53 +0530

    minor changes

--- member/includes/header.php
+++ member/includes/header.php
@@ -11,7 +11,11 @@
   <link rel="stylesheet" href="../../code/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
     <link rel="stylesheet" href="../../code/dist/css/adminlte.min.css">
-<!-- DataTables  & Plugins -->
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
+<!-- jquery validate -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- DataTables  & Plugins -->
 <script src="../../code/plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="../../code/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="../../code/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
